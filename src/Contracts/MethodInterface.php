0bb2ad3c17d00208e1d378ce2e0f2f7bd30e8b38
Mohammad Gufran <dogabhai@gmail.com>
2014-02-26 00:14:33 +0530

    added method setAuthentication()

--- src/Contracts/MethodInterface.php
+++ src/Contracts/MethodInterface.php
@@ -3,6 +3,14 @@
 interface MethodInterface {
 
     /**
+     * @param $loginId
+     * @param $transactionKey
+     *
+     * @return mixed
+     */
+    public function setAuthentication($loginId, $transactionKey);
+
+    /**
      * get the formatted xml data
      * @return string
      */
